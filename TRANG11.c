0b038167849d23c856603dd4ef1e79c5056d0bbb
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-10-31 11:34:04 +0700

    ghi chu vao day

--- TRANG11.c
+++ TRANG11.c
@@ -12,6 +12,16 @@ Giai va bien luan phuong trinh ax+b=0
 int main(){
 	float a,b;
 	printf("gia tri cua a va b lan luot la : ");
+	scanf("%f %f", &a, &b);
 	
+	if(a==0){
+		if(b==0){
+			printf("phuong trinh vo so nghiem"); 
+		} else{
+			printf("phuong trinh vo nghiem"); 
+		}
+		} else{
+			printf("nghiem cua he phuong trinh la : %f", -b/a); 
+		}  
 	return 0; 
 } 
