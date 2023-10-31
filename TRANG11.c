9f1ca7e4bc643578a492567e11529d43f7ea8929
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-11-01 04:52:32 +0700

    ghi chu vao day

--- TRANG11.c
+++ TRANG11.c
@@ -33,6 +33,7 @@ Bai 2
 Tinh tong s=1+2+3+...+n(n>0)
 */
 
+/*
 #include <stdio.h>
 
 int main(){
@@ -46,4 +47,26 @@ int main(){
 	} 
 	printf("tong cac so nguyen tu 1 den %d la : %d", n, i);
 	return 0; 
+} 
+*/
+
+/*
+Bai 3
+tinh tong s=1/2+2/3+3/4+..+(2n+1)/(2n+2) 
+n>0
+*/
+
+#include <stdio.h>
+
+int main(){
+	float n;
+	printf("gia tri cua n la:");
+	scanf("%f", &n);
+	
+	float i;
+	for(i=0;i<=n;i+=0,5){
+		i+=i; 
+	} 
+	printf("gia tri cua tong s la : %f", i);
+	return 0;  
 } 
