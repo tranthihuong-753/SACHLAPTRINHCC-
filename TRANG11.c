911c441e39667f19098d9ea08b23ad2970fe8cf2
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-10-31 11:27:06 +0700

    ghi chu vao day

---
+++ TRANG11.c
@@ -0,0 +1,17 @@
+/*
+Bai tap trang 11
+*/
+
+/* 
+Bai 1:
+Giai va bien luan phuong trinh ax+b=0
+*/
+
+#include <stdio.h> 
+
+int main(){
+	float a,b;
+	printf("gia tri cua a va b lan luot la : ");
+	
+	return 0; 
+} 
